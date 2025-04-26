08763b2afa6ba56122d4ce5ce8dcce71747746da
DeadPieGamer <felix@krog.dk>
2025-04-26 17:22:53 +0200

    Echo uses Gekkou model

--- GamesMaltheFelix/Assets/Scripts/Player/GhostReplayer.cs
+++ GamesMaltheFelix/Assets/Scripts/Player/GhostReplayer.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic; //siden vi bruger List<>
 public class GhostReplayer : MonoBehaviour
 {
     // Dette script skal bruges til at afspille en ghost replay af spilleren
+    [SerializeField] private Transform rigToMove;
 
-    [Header("List over frames")]
-    public List<PlayerFrameData> playbackData; //Skal den her væres static?
+    [HideInInspector] public List<PlayerFrameData> playbackData; //Skal den her væres static?
     private int currentIndex = 0; // Holder styr på hvilken frame vi er på i playbackData, aka spillerens "recording"
 
-    private bool isAttacking = false;
-    AnimatorStateInfo previousState; // En variable til at gemme den nuværende animation så vi kan gå tilbage til den når angrebet er færdigt
-    private Animator animator; // Animatoren på spøgelset der tracker attacket.
+    [SerializeField] private Animator myAnimator; // Animatoren på spøgelset der tracker attacket.
 
 
     void Start()
     {
-        animator = GetComponent<Animator>(); // Henter animatoren fra spøgelset så vi kan styre den
+        if (myAnimator == null)
+        {
+            if (!TryGetComponent(out myAnimator))
+                Debug.LogWarning($"Ghost {name} had no animator assigned and could not find any either. It will not function correctly");
+        }
+
+        if (rigToMove == null)
+            rigToMove = transform;
     }
     void FixedUpdate() // Bruger FixedUpdate for at matche frameraten på rigidbodiens bevægelse
     {
@@ -27,12 +32,12 @@ public class GhostReplayer : MonoBehaviour
         }
 
         var data = playbackData[currentIndex]; // Tager en frame fra playbackData listen og gemmer den i "data" variablen. Super sejt do
-        transform.position = data.position; // Sætter positionen på spøgelset til den position der er gemt i data variablen
-        transform.rotation = data.rotation; // Sætter rotationen på spøgelset til den rotation der er gemt i data variablen
+        rigToMove.position = data.position; // Sætter positionen på spøgelset til den position der er gemt i data variablen
+        rigToMove.rotation = data.rotation; // Sætter rotationen på spøgelset til den rotation der er gemt i data variablen
 
         if (data.didAttack)
         {
-            animator.SetTrigger("Attack"); // Sætter triggeren til at spille angrebs animationen
+            myAnimator.SetTrigger("Attack"); // Sætter triggeren til at spille angrebs animationen
         }
         
 
